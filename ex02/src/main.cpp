c089b449a740477fd5d5397f996c1df94c5a953e
ismo <ismo@example.com>
2026-05-11 20:51:13 +0200

    extra test ex02

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,6 +1,7 @@
 #include "MutantStack.hpp"
 #include "MutantStack.tpp"
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
@@ -8,22 +9,27 @@ int main()
 
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
 	}
-	std::stack<int> s(mstack);
+	{
+		auto it = std::find(mstack.begin(), mstack.end(), 737);
+
+		if (it != mstack.end())
+			std::cout << "found " << *it << std::endl;
+	}
 	return 0;
 }
